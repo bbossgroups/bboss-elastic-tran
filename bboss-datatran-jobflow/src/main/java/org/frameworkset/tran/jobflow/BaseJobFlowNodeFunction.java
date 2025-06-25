a0bef77d749a5904a9bf373e0199db18ba975139
大河 <yin-bp@163.com>
2025-06-25 23:43:29 +0800

    添加工作流自定义函数基础类

---
+++ bboss-datatran-jobflow/src/main/java/org/frameworkset/tran/jobflow/BaseJobFlowNodeFunction.java
@@ -0,0 +1,34 @@
+package org.frameworkset.tran.jobflow;
+/**
+ * Copyright 2025 bboss
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 工作流执行器函数基础类，一个节点只保存JobFlowNodeFunction的一个实例，因此一次调度执行完毕后，需要通过reset重置状态，release释放资源
+ * @author biaoping.yin
+ * @Date 2025/6/22
+ */
+public abstract class BaseJobFlowNodeFunction implements JobFlowNodeFunction{
+    protected JobFlowNode jobFlowNode;
+    /**
+     * 初始化构建节点函数实例时，只在构建工作流节点时调用一次
+     * @param jobFlowNode
+     */
+    public void init(JobFlowNode jobFlowNode){
+        this.jobFlowNode = jobFlowNode;
+    }
+
+   
+}
